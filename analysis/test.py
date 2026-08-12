e593abe5fa6feb7ba764f4bb388f6e8441e9a962
CGS9 <connorgsmith2020@gmail.com>
2026-08-11 23:10:36 -0400

    created analysis venv and requirements

---
+++ analysis/test.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import plotly.graph_objects as go
+
+bh = pd.read_csv("../output/AAPL_buyhold.csv")
+mac = pd.read_csv("../output/AAPL_crossover_50_200.csv")
+
+fig = go.Figure()
+fig.add_trace(go.Scatter(x=bh["Date"], y=bh["Equity"], name="Buy & Hold"))
+fig.add_trace(go.Scatter(x=mac["Date"], y=mac["Equity"], name="MA Crossover 50/200"))
+fig.update_layout(title="AAPL: Buy & Hold vs MA Crossover",
+                  xaxis_title="Date", yaxis_title="Equity ($)")
+fig.show()
