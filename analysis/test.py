2d8be85cbf40ba9366cb3f0a0f6eb59fc421e296
CGS9 <connorgsmith2020@gmail.com>
2026-08-13 16:40:33 -0400

    change path fiding to be more robust

--- analysis/test.py
+++ analysis/test.py
@@ -1,8 +1,15 @@
+from pathlib import Path
 import pandas as pd
 import plotly.graph_objects as go
 
-bh = pd.read_csv("../output/AAPL_buyhold.csv")
-mac = pd.read_csv("../output/AAPL_crossover_50_200.csv")
+# folder containing this script
+HERE = Path(__file__).resolve().parent
+# output is one level up from analysis/, then into output/
+OUT = HERE.parent / "output"
+
+bh = pd.read_csv(OUT / "AAPL_buyhold.csv")
+mac = pd.read_csv(OUT / "AAPL_crossover_50_200.csv")
+
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=bh["Date"], y=bh["Equity"], name="Buy & Hold"))
